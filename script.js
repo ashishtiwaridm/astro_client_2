7f74cccc80bafb1b88ba2b7811634be192b706db
Ashish Tiwari <143181468+ashishtiwaridm@users.noreply.github.com>
2024-03-13 22:43:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  var x = document.querySelector(".nav_bar");
+  if (x.classList.contains("responsive")) {
+    x.classList.remove("responsive");
+  } else {
+    x.classList.add("responsive");
+  }
+}
+
+// Slider Functionality
